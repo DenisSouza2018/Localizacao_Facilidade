b208f1094c43e778e8a28658188c32d71e93ddb1
Denis Souza <denis.tracoleal@gmail.com>
2020-09-03 00:48:47 -0300

    #1 Criando estrutura dicionario

--- main.py
+++ main.py
@@ -11,19 +11,54 @@ import numpy as np
 #     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class Cliente():
+    demanda = 0
+    custos = []
+
 
 def getInstancias(name,id):
 
     # Diretorio Denis
-    directory = 'C:/Users/denis/OneDrive/Documentos/Localizacao_e_facilidade/instancias/'+ name + id + '.txt'
+    directory = 'C:/Users/denis/OneDrive/Documentos/Localizacao_Facilidade/instancias/'+ name + id + '.txt'
+
+    # Variavies
+    cliente = Cliente()
+    vet_clientes = []
+    aux = []
 
     with open(directory, 'rb') as item:
-        nrows, ncols = [int(field) for field in item.readline().split()]
-        data = np.genfromtxt(item, dtype ="int32", max_rows = nrows)
-    return  data
+        #Processo para remover o matriz inicial
+        n_deposito, n_cliente = [int(field) for field in item.readline().split() ]
+        data = np.genfromtxt(item, dtype ="int32", max_rows = n_deposito)
+
+        #Estrutura responsavel por percorrer todo o arquivo
+        for x in item.readlines():
+
+            #Condição inicia para pegar a demanda de um cliente
+            if len(x.split()) == 1:
+                cliente.demanda = float(x)
+                cliente.custos = []
+                aux = []
+            else:
+                vet_custo = [float(field) for field in x.split()]
+                cliente.custos = vet_custo
+
+            # Condição responsavel por filtrar duplicatas
+            if len(cliente.custos) != 0:
+                aux += cliente.custos
+                if len(aux) == n_deposito:
+                    vet_clientes.append(aux)
 
 
-dado = getInstancias("cap","51")
+    return  vet_clientes
 
 
-print(dado)
+
+dado = getInstancias("cap","131")
+cont = 0
+
+#Estrutura responsavel por imprimir dicionario
+for c in dado:
+    print(cont,':',len(c), c)
+    cont+=1
+
