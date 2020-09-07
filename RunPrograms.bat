283dcaef073da03c0572778bc0e9f50b67904609
Isaias <immgermano@gmail.com>
2020-09-07 15:34:14 -0300

    Melhorando a estrutura modular
    Removido as instancias antigas, mudado nome das pastas para melhorar a modularizacao. Inserido o arquivo de .bat.

---
+++ RunPrograms.bat
@@ -0,0 +1,2 @@
+python main.py p1
+python main.py p20
--- main.py
+++ main.py
@@ -1,117 +1,20 @@
-# This is a sample Python script.
+import time
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from Localizacao_Facilidade.In_Out.read import read_inst as le
+from Localizacao_Facilidade.In_Out.write import write_file as escreve
 
-import numpy as np
+t_inicio = time.time()
 
+def main(inst):
+    path = 'c:/Meta/Localizacao_Facilidade/Instancias/' + inst
+    dados = le(path)
+    print(dados)
 
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
+    escreve(dados, inst)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-from Funcao_Escrita.write import write_file
+if __name__ == '__main__':
+    main(str(sys.argv[1]))
 
-
-class dado:
-    def __init__(self, demanda, custo):
-        self.demanda = demanda
-        self.custo = custo
-
-class Cliente():
-    demanda = 0
-    custos = []
-
-
-def getInstancias(name,id):
-
-    # Diretorio Denis
-    directory = 'C:/Users/denis/OneDrive/Documentos/Localizacao_Facilidade/instancias/'+ name + id + '.txt'
-
-    # Variavies
-    cliente = Cliente()
-    vet_clientes = []
-    aux = []
-    results = []
-
-    with open(directory, 'rb') as item:
-        #Processo para remover o matriz inicial
-        n_deposito, n_cliente = [int(field) for field in item.readline().split() ]
-        data = np.genfromtxt(item, dtype ="int32", max_rows = n_deposito)
-
-        #Estrutura responsavel por percorrer todo o arquivo
-        for x in item.readlines():
-
-            #Condição inicia para pegar a demanda de um cliente
-            if len(x.split()) == 1:
-                cliente.demanda = float(x)
-                cliente.custos = []
-                aux = []
-
-            else:
-                vet_custo = [float(field) for field in x.split()]
-                cliente.custos = vet_custo
-
-            # Condição responsavel por filtrar duplicatas
-            if len(cliente.custos) != 0:
-                aux += cliente.custos
-                if len(aux) == n_deposito:
-                    vet_clientes.append(aux)
-                    results.append(dado(cliente.demanda,aux))
-
-
-
-    return  results
-
-def imprimir(dados):
-    # Estrutura responsavel por imprimir dicionario
-    cont = 0
-    for c in dados:
-        print(cont, ':',len(c.custo), c.custo, ',',c.demanda)
-        cont += 1
-
-    print('-------------------------')
-    print(' ')
-
-dados = getInstancias("cap","41")
-imprimir(dados)
-
-dados = getInstancias("cap","42")
-imprimir(dados)
-
-dados = getInstancias("cap","43")
-imprimir(dados)
-
-dados = getInstancias("cap","44")
-imprimir(dados)
-
-dados = getInstancias("cap","51")
-imprimir(dados)
-
-dados = getInstancias("cap","81")
-imprimir(dados)
-
-dados = getInstancias("cap","82")
-imprimir(dados)
-
-dados = getInstancias("cap","83")
-imprimir(dados)
-
-dados = getInstancias("cap","84")
-imprimir(dados)
-
-dados = getInstancias("cap","131")
-imprimir(dados)
-
-dados = getInstancias("cap","132")
-imprimir(dados)
-
-dados = getInstancias("cap","133")
-imprimir(dados)
-
-dados = getInstancias("cap","134")
-imprimir(dados)
-
-## Função responsavel por gravar o resultado em um arquivo
-#write_file(dados,'instancia_2')
+t_total = time.time() - t_inicio
+print('{:.4f}s'.format(t_total))
